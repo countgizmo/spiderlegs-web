e94a661fc62bda400881b80a1573efae2f34d578
Evghenii Kondratenko <evghenii.kondratenko@merantix.com>
2020-06-06 06:13:23 +0200

    Prod build and deploy script
    Simply babashka script to build and copy some files. Still incomplete
    because the github push is done manually when I'm ready. This will probably be
    a github action in the future but for now it's good enough to have quick
    deployments of the new stuff!

---
+++ scripts/deploy.clj
@@ -0,0 +1,17 @@
+#!/usr/bin/env bb
+
+(require '[clojure.java.io :as io]
+         '[clojure.java.shell :as shell])
+
+
+(println "Build JS")
+(shell/sh "clj" "-m" "figwheel.main" "-bo" "prod")
+
+
+(println "Copy inde.html")
+(io/copy (io/file "resources/public/index.html")
+         (io/file "docs/index.html"))
+
+(println "Copy main.js")
+(io/copy (io/file "resources/public/js/prod/main.js")
+         (io/file "docs/js/main.js"))
