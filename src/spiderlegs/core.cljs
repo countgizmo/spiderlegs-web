bbd5589f4c7bde533e090900751acc6edae4f220
Evghenii Kondratenko <evghenii.kondratenko@merantix.com>
2020-06-06 15:14:42 +0200

    Add tailwind and a bunch of stylings to the views
    Mostly CSS changes and the whole NPM boilerplate + postcss configuration
    for the tailwindcss to work.

--- src/spiderlegs/core.cljs
+++ src/spiderlegs/core.cljs
@@ -17,8 +17,10 @@
 
 (defn ^:after-load mount
   []
-  (r/render [views/title-page]
-            (.getElementById js/document "app")))
+  (r/render
+   [:div {:class-name "mx-auto px-8 bg-gray-100 min-h-screen"}
+    [views/title-page]]
+   (.getElementById js/document "app")))
 
 (defn ^:export main
   []
--- src/spiderlegs/views.cljs
+++ src/spiderlegs/views.cljs
@@ -6,62 +6,89 @@
 
 (defn fretcell
   [string fret]
-  (let [width    60
-        height   30
-        x        (* fret width)
-        y        (* height string)
-        color    "black"
-        active?  (rf/subscribe [::subs/fretboard-position-active? string fret])]
+  (let [width   60
+        height  30
+        x       (* fret width)
+        y       (* height string)
+        color   "black"
+        active? (rf/subscribe [::subs/fretboard-position-active? string fret])]
     [:svg
-     [:rect {:width    width
-             :height   height
-             :stroke   color
-             :x        x :y y
-             :fill     "white"}]
-     [:line {:x1     x           :y1 y
-             :x2     (+ x width) :y2 y
-             :stroke "white"}]
+     [:rect {:width  width
+             :height height
+             :stroke "white"
+             :x      x
+             :y      y
+             :fill   "white"}]
      [:line {:x1     x           :y1 (+ y (/ height 2))
              :x2     (+ x width) :y2 (+ y (/ height 2))
              :stroke color}]
-     (when @active?
-       [:circle {:cx       (+ x (/ width 2))
-                 :cy       (+ y (/ height 2))
-                 :r        10
-                 :stroke   "purple"
-                 :fill     "purple"}])
-     [:line {:x1     x           :y1 (+ y height)
+     [:line {:x1     (+ x width) :y1 y
              :x2     (+ x width) :y2 (+ y height)
-             :stroke "white"}]]))
+             :stroke color}]
+     (when @active?
+       [:circle {:cx     (+ x (/ width 2))
+                 :cy     (+ y (/ height 2))
+                 :r      10
+                 :stroke "purple"
+                 :fill   "purple"}])]))
 
+
+(defn fret-dot
+  [fret]
+  [:circle {:r 5
+            :stroke "black"
+            :fill "gray"
+            :cx (- (+ 60 (* fret 60))
+                   30)
+            :cy 120}])
 
 (defn fretboard
   []
   [:div
-   [:svg  {:style {:border "1px solid"
-                   :background "white"
-                   :width "800px"
-                   :height "400px"}}
-    [:rect {:width 5 :height 200
+   [:svg {:style {:border     "1px solid"
+                  :background "white"
+                  :width      "800px"
+                  :height     "250px"}}
+    [:rect {:width  5
+            :height 200
             :stroke "black"
-            :x 55 :y 20}]
+            :x      55
+            :y      20}]
     (doall
      (for [string (range 1 7)
-           fret (range 1 13)]
-       ^{:key (str string ":" fret)} [fretcell string fret]))]])
+           fret   (range 1 13)]
+       ^{:key (str string ":" fret)} [fretcell string fret]))
+    (doall
+     (map (fn [fret]
+            ^{:key (str "fret-dot-" fret)} [fret-dot fret])
+          [3 5 7 9]))
+    [:circle {:r 5
+              :stroke "black"
+              :fill "gray"
+              :cx (- (+ 60 (* 12 60))
+                     30)
+              :cy 90}]
+    [:circle {:r 5
+              :stroke "black"
+              :fill "gray"
+              :cx (- (+ 60 (* 12 60))
+                     30)
+              :cy 150}]]])
 
 
 (defn answer-panel
   []
   (let [notes  @(rf/subscribe [::subs/all-notes])
         answer @(rf/subscribe [::subs/answer])]
-    (into [:div]
+    (into [:div
+           {:class-name "my-4"}]
           (doall
            (map
             (fn [note]
-              [:button {:class-name (if (= note answer)
-                                      "selected-answer-btn"
-                                      "answer-btn")
+              [:button {:class-name (str "bg-gray-700 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded mr-2 inline-block"
+                                         (if (= note answer)
+                                           "selected-answer-btn"
+                                           "answer-btn"))
                         :on-click #(rf/dispatch [::events/submit-answer note])} note])
             notes)))))
 
@@ -72,18 +99,22 @@
         expected-answer @(rf/subscribe [::subs/expected-answer])
         answered? (and answer expected-answer)]
     [:div
-     [:div "Name that note!"]
-     [:button
-      {:on-click #(rf/dispatch [::events/activate-random-fret-position 5])}
-      "Again!"]
+     {:class-name "pt-4 max-h-full"}
+     [:div {:class-name "mb-4 text-2xl text-gray-900"}
+      "Name that note!"]
      [fretboard]
      [answer-panel]
      (when answered?
        (if (= answer expected-answer)
-         [:div {:class-name "good-answer-indicator"} "Correct"]
-         [:div {:class-name "bad-answer-indicator"} "Incorrect"]))
-     (when answered?
-       [:div (str "The note is: " expected-answer)])]))
+         [:div {:class-name "text-green-700 font-bold text-xl"} "Correct!"]
+         [:div {:class-name "text-gray-900 font-bold text-xl"}
+          "The note is: "
+          [:span {:class-name "text-red-700 font-bold text-xl"} expected-answer]]))
+     [:button
+      {:on-click #(rf/dispatch [::events/activate-random-fret-position 5])
+       :class-name "mt-8 bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"}
+      "Again!"]
+     ]))
 
 
 (comment
