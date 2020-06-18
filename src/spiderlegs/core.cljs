1c2a935dc13c5f77e595d3f3837bc2eceb9885ad
Evghenii Kondratenko <evghenii.kondratenko@merantix.com>
2020-06-18 15:56:21 +0200

    Add different pages and routes
    Shuffled things around and implemented routing with the help of reitit.
    The app now has a simple nav bar and three pages:
    1. front page - currently empty, a landing page for root.
    2. fret hero - the fretboard note naming exercise.
    3. random note - should be renamed to something else, basically gives you a
    random fret and a random string and then it's up to you to do things with
    this information :)

--- src/spiderlegs/core.cljs
+++ src/spiderlegs/core.cljs
@@ -1,6 +1,10 @@
 (ns ^:figwheel-hooks spiderlegs.core
-  (:require [reagent.core :as r]
-            [re-frame.core :as rf]
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]
+            [re-frame.core :as re-frame]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            [reitit.coercion.spec :as rss]
             [spiderlegs.events :as events]
             [spiderlegs.views :as views]))
 
@@ -14,16 +18,53 @@
     (enable-console-print!)
     (println "dev mode")))
 
+(defonce match (r/atom nil))
+
+(defn current-page []
+  [:div
+   {:class-name "mx-auto bg-gray-100 min-h-screen"}
+   [:header
+    {:class-name "bg-gray-900 text-gray-300 py-2 flex"}
+    [:div
+     [:a
+      {:href "/#/random-note"
+       :class-name "hover:text-gray-100 ml-10"}
+      "Random Note"]]
+    [:div
+     [:a
+      {:href "/#/fret-hero"
+       :class-name "hover:text-gray-100 cursor-pointer ml-10"}
+      "Fret Hero"]]]
+   (when @match
+     (let [view (:view (:data @match))]
+       [view @match]))])
+
+
+(def routes
+  [["/"
+    {:name ::frontpage
+     :view views/frontpage}]
+
+   ["/fret-hero"
+    {:name ::fret-hero
+     :view views/fret-hero}]
+
+   ["/random-note"
+    {:name ::random-note
+     :view views/random-note}]])
 
 (defn ^:after-load mount
   []
-  (r/render
-   [:div {:class-name "mx-auto px-8 bg-gray-100 min-h-screen"}
-    [views/title-page]]
+  (rfe/start!
+   (rf/router routes {:data {:coercion rss/coercion}})
+   (fn [m] (reset! match m))
+   {:use-fragment true})
+  (rdom/render
+   [current-page]
    (.getElementById js/document "app")))
 
 (defn ^:export main
   []
-  (rf/dispatch-sync [::events/initialise-db])
+  (re-frame/dispatch-sync [::events/initialise-db])
   (dev-setup)
   (mount))
--- src/spiderlegs/views.cljs
+++ src/spiderlegs/views.cljs
@@ -47,6 +47,7 @@
 (defn fretboard
   []
   [:div
+   {:class-name "mx-8"}
    [:svg {:style {:border     "1px solid"
                   :background "white"
                   :width      "800px"
@@ -83,7 +84,7 @@
   (let [notes  @(rf/subscribe [::subs/all-notes])
         answer @(rf/subscribe [::subs/answer])]
     (into [:div
-           {:class-name "my-4"}]
+           {:class-name "my-4 mx-8"}]
           (doall
            (map
             (fn [note]
@@ -95,28 +96,60 @@
             notes)))))
 
 
-(defn title-page
+
+
+
+(defn fret-hero
   []
   (let [answer          @(rf/subscribe [::subs/answer])
         expected-answer @(rf/subscribe [::subs/expected-answer])
         answered?       (and answer expected-answer)]
     [:div
      {:class-name "pt-4 max-h-full"}
-     [:div {:class-name "mb-4 text-2xl text-gray-900"}
+     [:div {:class-name "mb-4 text-2xl text-gray-900 mx-8"}
       "Name that note!"]
      [fretboard]
      [answer-panel]
      (when answered?
        (if (= answer expected-answer)
-         [:div {:class-name "text-green-700 font-bold text-xl"} "Correct!"]
+         [:div {:class-name "text-green-700 font-bold text-xl mx-8"} "Correct!"]
          [:div {:class-name "text-gray-900 font-bold text-xl"}
           "The note is: "
           [:span {:class-name "text-red-700 font-bold text-xl"} expected-answer]]))
      [:button
       {:on-click   #(rf/dispatch [::events/activate-random-fret-position 5])
-       :class-name "mt-8 bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"}
-      "Again!"]
-     ]))
+       :class-name "mt-8 bg-gray-700 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded mb-4 mx-8"}
+      "Again!"]]))
+
+
+(defn random-note
+  []
+  (let [{:keys [fret string]} @(rf/subscribe [::subs/random-string-fret])]
+    [:div
+     {:class-name "mt-10 ml-20"}
+     [:span
+      {:class-name "mx-8 text-2xl text-gray-700"}
+      "Fret: "
+      [:span
+       {:class-name "font-bold text-3xl text-gray-900"}
+       fret]]
+     [:span
+      {:class-name "mx-8 text-2xl text-gray-700"}
+      "String: "
+      [:span
+       {:class-name "font-bold text-3xl text-gray-900"}
+       string]]
+     [:button
+      {:on-click   #(rf/dispatch [::events/generate-rand-string-fret])
+       :class-name "block mt-8 bg-gray-700 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded mb-4 mx-8"}
+      "Hit Me!"]]))
+
+
+(defn frontpage
+  []
+  [:div
+   "This is home!"])
+
 
 
 (comment
